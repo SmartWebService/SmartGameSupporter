02fc9427d12672a85b5d8b5e8a648f5298c821df
Jason <bsball123km@nate.com>
2019-12-04 17:27:12 +0900

    RPS FUNCTION VER1

--- game/views.py
+++ game/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 def in_game(request, game_code):
     # return render(request, 'five-poker/gamescreen.html')
 
-    return render(request, 'RPS/RSPmain.html')
+    return render(request, 'RPS/RSPresult.html')
