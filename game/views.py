537893d8bc453c6dd754208978cb4ef55d5f3ead
ParkJeongseop <parkjeongseop@parkjeongseop.com>
2019-12-04 16:25:30 +0900

    RPS 장고 템플릿에 반영

--- game/views.py
+++ game/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def in_game(request, game_code):
-    return render(request, 'five-poker/gamescreen.html')
+    # return render(request, 'five-poker/gamescreen.html')
+
+    return render(request, 'RPS/PSPmain.html')
